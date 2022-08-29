01aab38e39db44998d47d4e25e23725bd7d4fa00
Abril Aran <abrilaranr@gmail.com>
2022-08-29 16:33:09 +0200

    Traer el otro repositorio

---
+++ script.js
@@ -0,0 +1,32 @@
+const KcalOutut = getElementById('recipeKcalOutput');
+const priceOutput = getElementById('recipePriceOutput');
+
+
+function deleteIngredient(ingredient, element){
+
+}
+
+function getIngredientInfo(ingredient, element){
+
+}
+
+function calcIngredientKcal(ingredient, element){
+
+}
+
+function getIngredientMeasure(ingredient, element){
+
+}
+
+function setIngredientId(ingredient, element){
+    
+}
+
+function deleteRecipe(){
+
+}
+
+function saveRecipe(){
+
+}
+
